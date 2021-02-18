4ee8d2466316f767d2d606c6e22ce80435ac1a60
Idun420 <74796878+Idun420@users.noreply.github.com>
2021-02-18 12:47:42 +0100

    added comments and fixed a small bug

--- main.py
+++ main.py
@@ -1,42 +1,41 @@
 from PIL import Image, ImageDraw
 import math
 
-im = Image.new("RGBA", (500,500), (0,0,0,255)) # intialising the image class
+im = Image.new("RGBA", (500,500), (0,0,0,255)) # Intialising the image class
 draw = ImageDraw.Draw(im) # Getting ready
 
-slices = int(input("how many pizza slices would you like \n"))
+slices = int(input("how many pizza slices would you like \n")) # Asking the user
 
-x1 = 150
-y1 = 150
+x1 = 150 # Determining where the circe should be
+y1 = 150 # The same as above
 x2 = 400
 y2 = 400
-ang = 0
-constAng = 360 / slices
-a = (x2 - x1) / 2
-b = (y2 - y1) / 2
-rad = math.radians(ang)
-mid = ((x1 + x2) / 2, (y1 + y2) / 2)
+ang = 0 # A variable to keep track of were the last slice were made
+constAng = 360 / slices # How big each slice have to be
+a = (x2 - x1) / 2 # The vertical radius
+b = (y2 - y1) / 2 # The horisontal radius
+rad = math.radians(ang) # Converting angle to radians cause python math
+mid = ((x1 + x2) / 2, (y1 + y2) / 2) # Finding the center of the circle
 
-dist = lambda a, b, rad: a * b / math.sqrt((b * math.cos(rad)) ** 2 + (a * math.sin(rad)) ** 2)
-point = lambda distance, mid, rad, ang: (mid[0] + math.cos(rad) * distance, mid[1] - math.sin(rad) * distance)
+dist = lambda a, b, rad: a * b / math.sqrt((b * math.cos(rad)) ** 2 + (a * math.sin(rad)) ** 2) # The distance from the center of the circle the circle at a specific angle
+point = lambda distance, mid, rad, ang: (mid[0] + math.cos(rad) * distance, mid[1] - math.sin(rad) * distance) # Using trigonomitri to get the corodinats of the edge of the circle
 
-draw.ellipse(
-(x1,y1,x2,y2),
-outline = (0,255,0)
+draw.ellipse( # Draws the ellipse
+(x1,y1,x2,y2), # sets the corodinatsof the ellipse
+outline = (0,255,0) # makes it green
 )
 
-for i in range(slices + 1):
-    distance = dist(a, b, rad)
-    tempPoint = point(distance, mid, rad, ang)
+for i in range(slices): # Start the loop
+    distance = dist(a, b, rad) # Run the dist funktion to get the distance to the edge
+    tempPoint = point(distance, mid, rad, ang) # Runs the point funktion to get the corodinats of the edge
 
-    draw.line(
-        (mid, tempPoint),
-        fill = (0,255,0)
+    draw.line( # Draws the line
+        (mid, tempPoint), # The corodinats of the line
+        fill = (0,255,0) # Making it gren
     )
 
-    rad = math.radians(ang)
-    ang += constAng
+    ang += constAng # making the angle bigger sa the next slice is ready to be cut
+    rad = math.radians(ang) # convertin the angle to radians
 
 
-im.save("image.png", quality = 100)
-
+im.save("image.png", quality = 100) # Saving the image
